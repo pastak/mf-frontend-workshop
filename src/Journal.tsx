37cde830bca667456559bb3da6a85b5a6c660dd3
pastak <pasta0915@gmail.com>
2022-02-20 18:16:57 +0900

    createInitialDataで初期データを生成できるようにする

--- src/Journal.tsx
+++ src/Journal.tsx
@@ -2,8 +2,15 @@ import { useState } from "react"
 import { Row } from "./Row";
 import { JournalData } from "./types/journal"
 
+const createInitialData = () => ({
+  debitSubject: '',
+  debitValue: 0,
+  creditSubject: '',
+  creditValue: 0,
+});
+
 export const Journal = () => {
-  const [data, setData] = useState<JournalData>([]);
+  const [data, setData] = useState<JournalData>([createInitialData()]);
 
   return <>{
     data.map((d) => <Row data={d} update={() => {/* 一旦空の関数を渡してごまかす */}} />)
