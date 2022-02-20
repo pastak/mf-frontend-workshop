9e949cec65bcde4c4e460a9fa61b385b815a229f
pastak <pasta0915@gmail.com>
2022-02-21 00:43:23 +0900

    onChange仕掛けて回る

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { JournalRow } from "./types/journal";
 
 type ItemsAPI = Readonly<string[]>
 
 type RowProps = Readonly<{
   data: JournalRow
-  update: (newData: JournalRow) => void
+  update: (newData: Partial<JournalRow>) => void
 }>
-export const Row: React.VFC<RowProps> = () => {
+export const Row: React.VFC<RowProps> = ({ update }) => {
   const [items, setItems] = useState<ItemsAPI | undefined>(undefined);
 
   useEffect(() => {
@@ -19,13 +19,21 @@ export const Row: React.VFC<RowProps> = () => {
   }, []);
 
   return <div role="row">
-    <select>{
+    <select onChange={(e: React.SyntheticEvent<HTMLSelectElement>) => update({
+      debitSubject: e.currentTarget.value
+    })}>{
       items?.map(item => <option value={item}>{item}</option>)
     }</select>
-    <input type='number' />
-    <select>{
+    <input type='number' onChange={(e: React.SyntheticEvent<HTMLInputElement>) => update({
+      debitValue: e.currentTarget.valueAsNumber
+    })}/>
+    <select onChange={(e: React.SyntheticEvent<HTMLSelectElement>) => update({
+      creditSubject: e.currentTarget.value
+    })}>{
       items?.map(item => <option value={item}>{item}</option>)
     }</select>
-    <input type='number' />
+    <input type='number' onChange={(e: React.SyntheticEvent<HTMLInputElement>) => update({
+      creditValue: e.currentTarget.valueAsNumber
+    })}/>
   </div>
 }
