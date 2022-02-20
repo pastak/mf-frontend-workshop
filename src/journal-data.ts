bb2a902ef0dcdec65c8df9220487ae5a047656eb
pastak <pasta0915@gmail.com>
2022-02-21 00:15:31 +0900

    useJournalDataを実装

--- src/journal-data.ts
+++ src/journal-data.ts
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import { JournalData, JournalRow } from "./types/journal"
+
+const createInitialData = () => ({
+  id: crypto.randomUUID(),
+  debitSubject: '',
+  debitValue: 0,
+  creditSubject: '',
+  creditValue: 0,
+});
 
 export const useJournalData = (): Readonly<{
   data: JournalData,
   updateDataById: (id: string, newData: Partial<JournalRow>) => void;
   addRow: () => void;
 }> => {
+  const [data, setData] = useState<JournalData>([createInitialData()]);
+  
   return {
-    data: [],
-    updateDataById: (id: string, newData: Partial<JournalRow>) => {},
-    addRow: () => {}
+    data,
+    updateDataById: (id: string, newData: Partial<JournalRow>) => {
+      setData(prev => prev.map((d) => d.id === id ? { ...d, ...newData } : d))
+    },
+    addRow: () => { setData((prev) => [...prev, createInitialData()]) }
   }
 }
