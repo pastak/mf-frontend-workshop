e419a6c7f3ca981830381ae4efe45aba5a3c9968
pastak <pasta0915@gmail.com>
2022-02-20 18:06:37 +0900

    JSX部分を素朴に書いてみる

--- src/Row.tsx
+++ src/Row.tsx
@@ -4,4 +4,12 @@ type RowProps = Readonly<{
   data: JournalRow
   update: (newData: JournalRow) => void
 }>
-export const Row: React.VFC<RowProps> = () => null; //一旦中身無いので、nullを返しておく
+export const Row: React.VFC<RowProps> = () => {
+
+  return <div>
+    <select>{/* ここに課目が並ぶ */}</select>
+    <input type='number' />
+    <select>{/* ここに課目が並ぶ */}</select>
+    <input type='number' />
+  </div>
+}
