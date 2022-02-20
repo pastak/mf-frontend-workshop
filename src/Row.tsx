e8b29a1d4ffbb5e0640145b2554b9f524030a8aa
pastak <pasta0915@gmail.com>
2022-02-21 00:48:55 +0900

    defaultValueを渡して初期値を設定する

--- src/Row.tsx
+++ src/Row.tsx
@@ -7,7 +7,7 @@ type RowProps = Readonly<{
   data: JournalRow
   update: (newData: Partial<JournalRow>) => void
 }>
-export const Row: React.VFC<RowProps> = ({ update }) => {
+export const Row: React.VFC<RowProps> = ({ data, update }) => {
   const [items, setItems] = useState<ItemsAPI | undefined>(undefined);
 
   useEffect(() => {
@@ -21,19 +21,19 @@ export const Row: React.VFC<RowProps> = ({ update }) => {
   return <div role="row">
     <select onChange={(e: React.SyntheticEvent<HTMLSelectElement>) => update({
       debitSubject: e.currentTarget.value
-    })}>{
+    })} defaultValue={data.debitSubject}>{
       items?.map(item => <option value={item}>{item}</option>)
     }</select>
     <input type='number' onChange={(e: React.SyntheticEvent<HTMLInputElement>) => update({
       debitValue: e.currentTarget.valueAsNumber
-    })}/>
+    })} defaultValue={data.debitValue}/>
     <select onChange={(e: React.SyntheticEvent<HTMLSelectElement>) => update({
       creditSubject: e.currentTarget.value
-    })}>{
+    })} defaultValue={data.creditSubject}>{
       items?.map(item => <option value={item}>{item}</option>)
     }</select>
     <input type='number' onChange={(e: React.SyntheticEvent<HTMLInputElement>) => update({
       creditValue: e.currentTarget.valueAsNumber
-    })}/>
+    })} defaultValue={data.creditValue} />
   </div>
 }
