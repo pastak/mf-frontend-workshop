7c6d07e86150b2090b1fcb844725e39411ab9aaa
pastak <pasta0915@gmail.com>
2022-02-20 19:39:38 +0900

    "./Row"をモックする

--- src/Journal.test.tsx
+++ src/Journal.test.tsx
@@ -1,6 +1,10 @@
 import { screen, render, fireEvent } from "@testing-library/react";
 import {Journal} from "./Journal";
 
+jest.mock("./Row", () => ({
+  Row: () => <div role="row"></div>
+}));
+
 test('「行を追加」ボタンをクリックすると1行増える', () => {
   render(<Journal />);
   const button = screen.getByText('行を追加');
