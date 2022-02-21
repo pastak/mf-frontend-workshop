a93c0f3bf63a17eaa0565d377527caa8dd6aae81
pastak <pasta0915@gmail.com>
2022-02-21 13:24:09 +0900

    fetch('/api/subjects.json')して課目の一覧を取得する

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
 import { JournalRow } from "./types/journal";
+
+type ItemsAPI = Readonly<string[]>
 
 type RowProps = Readonly<{
   data: JournalRow
   update: (newData: JournalRow) => void
 }>
 export const Row: React.VFC<RowProps> = () => {
+  const [items, setItems] = useState<ItemsAPI | undefined>(undefined);
+
+  useEffect(() => {
+    fetch('/api/items.json')
+      .then((res) => res.json())
+      .then((json: ItemsAPI) => {
+        setItems(json);
+      });
+  }, []);
 
   return <div role="row">
-    <select>{/* ここに課目が並ぶ */}</select>
+    <select>{
+      items?.map(item => <option value={item}>{item}</option>)
+    }</select>
     <input type='number' />
-    <select>{/* ここに課目が並ぶ */}</select>
+    <select>{
+      items?.map(item => <option value={item}>{item}</option>)
+    }</select>
     <input type='number' />
   </div>
 }
